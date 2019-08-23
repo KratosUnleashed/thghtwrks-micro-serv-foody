0356c1b81dcbe2aff2a7c9f368f0108707757a2d
KratosUnleashed <anandrajpro@gmail.com>
2019-08-23 17:26:50 +0530

    updating order payment service

---
+++ order-payment-service/src/main/java/com/thghtwrks/workshop/orderpaymentservice/domain/Cart.java
@@ -0,0 +1,10 @@
+package com.thghtwrks.workshop.orderpaymentservice.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cart {
+	
+	private long id;
+
+}
---
+++ order-payment-service/src/main/java/com/thghtwrks/workshop/orderpaymentservice/domain/Order.java
@@ -0,0 +1,8 @@
+package com.thghtwrks.workshop.orderpaymentservice.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Order {
+
+}
