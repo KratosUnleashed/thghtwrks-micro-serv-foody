8035ae08c90e63ee50c95989b0ca54a8004947cc
KratosUnleashed <anandrajpro@gmail.com>
2019-08-23 16:32:05 +0530

    adding project templates

---
+++ foody-metadata-service/src/main/java/com/thghtwrks/workshop/foodymetadataservice/FoodyMetadataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thghtwrks.workshop.foodymetadataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodyMetadataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodyMetadataServiceApplication.class, args);
+	}
+
+}
---
+++ order-payment-service/src/test/java/com/thghtwrks/workshop/orderpaymentservice/OrderPaymentServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thghtwrks.workshop.orderpaymentservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderPaymentServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
